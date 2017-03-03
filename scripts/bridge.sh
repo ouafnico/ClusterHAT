a5a1ac5e2eb71742d8ce189332f0f19162a96b8f
Nicolas Repentin <nicolas@shivaserv.fr>
2017-03-03 09:42:10 +0100

    update bridge.sh

--- scripts/bridge.sh
+++ scripts/bridge.sh
@@ -8,6 +8,5 @@ iptables -t mangle -X
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i br0 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o br0 -m state --state ESTABLISHED,RELATED -j ACCEPT 
-iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0 -j MASQUERADE
